59b8f9b54d6662162cd01e2361e19e8d1452611e
liuchuangao <liuchuangao@tenda.cn>
2020-07-23 10:25:29 +0800

    init

---
+++ demo1/打包html资源/webpack.config.js
@@ -0,0 +1,24 @@
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'built.js',
+    path: resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+    ]
+  },
+  plugins: [
+    // plugins的配置
+    // html-webpack-plugin
+    // 功能：默认会创建一个空的HTML，自动引入打包输出的所有资源（JS/CSS）
+    // 需求：需要有结构的HTML文件
+    new HtmlWebpackPlugin({
+      // 复制 './src/index.html' 文件，并自动引入打包输出的所有资源（JS/CSS）
+      template: './src/index.html'
+    })
+  ],
+  mode: 'development'
+};
---
+++ demo1/打包样式资源/webpack.config.js
@@ -0,0 +1,30 @@
+const { resolve } = require('path');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'built.js',
+    path: resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+  ],
+  mode: 'development',
+}
