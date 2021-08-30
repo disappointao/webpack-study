1cde3f3bd7cedc18f4dd5f76c74239ba8c32108f
liuchuangao <1147608791@qq.com>
2021-08-30 16:13:52 +0800

    [webpack5][基础 typescript的处理]

--- webpack5/webpack sourceMap/src/main.js
+++
@@ -1,7 +0,0 @@
-import "./js/component";
-
-function sum (a, b){
-  return a + b
-}
-
-sum(1,2)
--- webpack5/webpack 实用插件/postcss.config.js
+++
@@ -1,7 +0,0 @@
-//也可在webpack配置文件中导入loader时进行配置
-module.exports = {
-  plugins: [
-    //postcss-preset-env将一些现代的CSS特性，转成大多数浏览器认识的CSS，并且会根据目标浏览器或者运行时环 境添加所需的polyfill,相当于已经内置了autoprefixer
-    require("postcss-preset-env")
-  ]
-}
